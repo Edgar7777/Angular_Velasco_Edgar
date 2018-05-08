95b095663bd6d4449c64c1b49bb2342d9f9500bc
Edgar7777 <losthedgar@hotmail.com>
2018-05-08 15:35:25 -0500

    commit modulos

--- spoiler-app/src/app/app.module.ts
+++ spoiler-app/src/app/app.module.ts
@@ -15,4 +15,21 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  nombre:'Edgar';
+  apellido:'Velasco';
+  edad:'28';
+  sueldo:1;
+  fechaNacimiento = new Date('1989-06-10');
+  nombreBoton = 'Cambiar color';
+  nombreClase = 'sa-titulo';
+
+  cambiarClaseDeTitulo(){
+    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+    if(nombreClaseEsTitulo){
+      this.nombreClase='sa-subtitulo';
+    }else{
+      this.nombreClase='sa-titulo';
+    }
+  }
+}
