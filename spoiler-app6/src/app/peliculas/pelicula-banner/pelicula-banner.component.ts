6faddf32712b6697211690943d06c89f07ff2f5c
Edgar7777 <losthedgar@hotmail.com>
2018-05-22 06:30:02 -0500

    modificaciones y botones

--- spoiler-app6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ spoiler-app6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class PeliculaBannerComponent implements OnInit {
 
   public title = 'Spoiler CTM';
-  public descripcion = 'Videos de Estreno y Proximamente';
+  public descripcionPelicula = 'Imagen de la pelicula Overlord';
+  nombrePelicula = 'OverLord';
   public urlImagen = 'http://vignette4.wikia.nocookie.net/overlordmaruyama/images/c/cd/Albedo.png';
   public urlImagen1 = 'https://vignette.wikia.nocookie.net/deathbattlefanon/images/5/57/Shalltear_Bloodfallen.jpeg/revision/latest?cb=20150720194730';
   public urlImagen2 = 'https://www.animeclick.it/immagini/anime/Overlord/cover/Overlord-cover.jpg';
@@ -19,6 +20,13 @@ export class PeliculaBannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if (this.esEstreno) {
+      this.textoEstreno = 'Estreno';
+      this.claseEstreno = 'sa-color-estado-rosado';
+    } else {
+      this.textoEstreno = 'Proximamente';
+      this.claseEstreno = 'sa-color-estado-amarillo';
+    }
 
   }
 
