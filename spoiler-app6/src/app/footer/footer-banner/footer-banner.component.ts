93ef6efefd97abc01ec3f611e17caa8cb7a8f4b5
Edgar7777 <losthedgar@hotmail.com>
2018-05-22 14:03:57 -0500

    agregar ngFor para reducir código

--- spoiler-app6/src/app/footer/footer-banner/footer-banner.component.ts
+++ spoiler-app6/src/app/footer/footer-banner/footer-banner.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterBannerComponent implements OnInit {
 
+  urlRedSocial1 = 'http://cioal.com/wp-content/uploads/sites/2/2014/11/logo-facebook-720x717.jpg';
+  urlRedSocial2 = 'https://pbs.twimg.com/profile_images/875087697177567232/Qfy0kRIP_400x400.jpg';
+
   constructor() { }
 
   ngOnInit() {
--- spoiler-app6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ spoiler-app6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -5,15 +5,123 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
-export class PeliculaBannerComponent implements OnInit {
+export class PeliculaBannerComponent implements OnInit{
+
+  esEstreno = false;
 
   public title = 'Spoiler CTM';
-  public descripcionPelicula = 'Imagen de la pelicula Overlord';
-  nombrePelicula = 'OverLord';
-  public urlImagen = 'http://vignette4.wikia.nocookie.net/overlordmaruyama/images/c/cd/Albedo.png';
-  public urlImagen1 = 'https://vignette.wikia.nocookie.net/deathbattlefanon/images/5/57/Shalltear_Bloodfallen.jpeg/revision/latest?cb=20150720194730';
-  public urlImagen2 = 'https://www.animeclick.it/immagini/anime/Overlord/cover/Overlord-cover.jpg';
-  esEstreno = false;
+  // public descripcionPelicula = 'Imagen de la pelicula Overlord';
+  // nombrePelicula = 'OverLord';
+  // public urlImagen = 'http://vignette4.wikia.nocookie.net/overlordmaruyama/images/c/cd/Albedo.png';
+  // public urlImagen1 = 'https://vignette.wikia.nocookie.net/deathbattlefanon/images/5/57/Shalltear_Bloodfallen.jpeg/revision/latest?cb=20150720194730';
+  // public urlImagen2 = 'https://www.animeclick.it/immagini/anime/Overlord/cover/Overlord-cover.jpg';
+  //esEstreno = false;
+
+  arregloPeliculasProximamente = [
+    {
+      descripcionPelicula: 'Imagen de la pelicula Overlord',
+      nombrePelicula: 'OverLord',
+      esEstreno: false,
+      urlImagen: 'http://vignette4.wikia.nocookie.net/overlordmaruyama/images/c/cd/Albedo.png'
+
+    },
+    {
+      descripcionPelicula: 'Imagen Pelicula No Game No Life 0',
+      nombrePelicula: 'No Game No Life 0',
+      esEstreno: false,
+      urlImagen: 'http://kancha.pe/wp-content/uploads/2018/03/nogamenolife-770x514.jpg'
+    },
+
+  ]
+
+  arregloPeliculasEstreno = [
+
+    {
+      nombrePelicula: 'Sword Art Online Ordinal Scale',
+      esEstreno: true,
+      urlImagen: 'https://media.redadn.es/imagenes/animanga-0_317805.jpg'
+
+    },
+    {
+      nombrePelicula: 'mazinger Z Infinity',
+      esEstreno: true,
+      urlImagen: 'https://static.cinepolis.com/img/peliculas/26793/1/1/26793.jpg'
+
+    },
+    {
+      nombrePelicula: 'Dragon Ball Super Saiyanjin Origins',
+      esEstreno: true,
+      urlImagen: 'https://media.metrolatam.com/2018/05/17/daoi7oauqaaxotf-d3f1d5cc0d9cf40ee3e4ec2b2e719f48.jpglarge'
+
+    },
+    {
+      nombrePelicula: 'Detective Conan 2018',
+      esEstreno: true,
+      urlImagen: 'https://ia.media-imdb.com/images/M/MV5BNjMzMDA3MDYtNTNlMS00YTc3LTlkYmItYjViMjZjY2U1YzNmXkEyXkFqcGdeQXVyMjY2OTU0MTg@._V1_.jpg'
+
+    },
+
+  ]
+
+  arregloPeliculasAgregadasUltimamenteIzquierda = [
+
+    {
+      nombrePelicula: 'Sword Art Online Ordinal Scale',
+      esEstreno: true,
+      urlImagen: 'https://media.redadn.es/imagenes/animanga-0_317805.jpg'
+
+    },
+    {
+      nombrePelicula: 'mazinger Z Infinity',
+      esEstreno: true,
+      urlImagen: 'https://static.cinepolis.com/img/peliculas/26793/1/1/26793.jpg'
+
+    },
+    {
+      nombrePelicula: 'Dragon Ball Super Saiyanjin Origins',
+      esEstreno: true,
+      urlImagen: 'https://media.metrolatam.com/2018/05/17/daoi7oauqaaxotf-d3f1d5cc0d9cf40ee3e4ec2b2e719f48.jpglarge'
+
+    },
+    {
+      nombrePelicula: 'Detective Conan 2018',
+      esEstreno: true,
+      urlImagen: 'https://ia.media-imdb.com/images/M/MV5BNjMzMDA3MDYtNTNlMS00YTc3LTlkYmItYjViMjZjY2U1YzNmXkEyXkFqcGdeQXVyMjY2OTU0MTg@._V1_.jpg'
+
+    },
+
+  ]
+
+  arregloPeliculasAgregadasUltimamenteDerecha = [
+
+    {
+      nombrePelicula: 'Sword Art Online Ordinal Scale',
+      esEstreno: true,
+      urlImagen: 'https://media.redadn.es/imagenes/animanga-0_317805.jpg'
+
+    },
+    {
+      nombrePelicula: 'mazinger Z Infinity',
+      esEstreno: true,
+      urlImagen: 'https://static.cinepolis.com/img/peliculas/26793/1/1/26793.jpg'
+
+    },
+    {
+      nombrePelicula: 'Dragon Ball Super Saiyanjin Origins',
+      esEstreno: true,
+      urlImagen: 'https://media.metrolatam.com/2018/05/17/daoi7oauqaaxotf-d3f1d5cc0d9cf40ee3e4ec2b2e719f48.jpglarge'
+
+    },
+    {
+      nombrePelicula: 'Detective Conan 2018',
+      esEstreno: true,
+      urlImagen: 'https://ia.media-imdb.com/images/M/MV5BNjMzMDA3MDYtNTNlMS00YTc3LTlkYmItYjViMjZjY2U1YzNmXkEyXkFqcGdeQXVyMjY2OTU0MTg@._V1_.jpg'
+
+    },
+
+  ]
+
+
   textoEstreno: string;
   claseEstreno: string;
 
@@ -23,13 +131,10 @@ export class PeliculaBannerComponent implements OnInit {
     if (this.esEstreno) {
       this.textoEstreno = 'Estreno';
       this.claseEstreno = 'sa-color-estado-rosado';
-    } else {
+
+    } else{
       this.textoEstreno = 'Proximamente';
       this.claseEstreno = 'sa-color-estado-amarillo';
     }
-
   }
-
-
-
 }
